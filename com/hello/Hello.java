dc5244ccd166eab8a55d7eca2e7b4a2a9e1054a4
higebre16 <higebre16@gmail.com>
2020-09-19 12:28:23 -0500

    modified Hello classs

--- com/hello/Hello.java
+++ com/hello/Hello.java
@@ -2,6 +2,6 @@ package com.hello;
 
 public class Hello{
     public static void main(String [] a){
-         System.out.println("Hello Gebremedhin.");
+         System.out.println("Hello Gebremedhinn Taddese.");
     }
 }
